ba9dbe4f0eb3595292e4b9e090c4df5c2eb3a012
nic4cio <gabrielnn_1@live.com>
2023-05-17 21:09:00 -0300

    add: vt line and vn line

--- LoadMesh.py
+++ LoadMesh.py
@@ -7,6 +7,14 @@ class LoadMesh(Mesh):
     def __init__(self, filename, draw_type):
         self.vertices = []
         self.triangles = []
+
+        ## adding vnormals and uvs
+
+        self.normals = []
+        self.normal_ind = []
+        self.uvs = []
+        self.uvs_ind = []
+
         self.filename = filename
         self.draw_type = draw_type
         self.load_drawing()
@@ -18,9 +26,31 @@ class LoadMesh(Mesh):
                 if line[:2] == "v ":
                     vx, vy, vz = [float(value) for value in line[2:].split()]
                     self.vertices.append((vx, vy, vz))
+                if line[:2] == "vn":
+                    vx, vy, vz == [float(value) for value in line[3:].split()]
+                    self.normals.append((vx, vy, vz))
+                if line[:2] == "vt":
+                    vx, vy == [float(value) for value in line[3:].split()]
+                    self.uvs.append((vx, vy))
                 if line[:2] == "f ":
                     t1, t2, t3 = [value for value in line[2:].split()]
                     self.triangles.append([int(value) for value in t1.split('/')][0]-1)
                     self.triangles.append([int(value) for value in t2.split('/')][0]-1)
                     self.triangles.append([int(value) for value in t3.split('/')][0]-1)
+
+                    self.uvs_ind.append([int(value) for value in t1.split('/')][1] - 1)
+                    self.uvs_ind.append([int(value) for value in t2.split('/')][1] - 1)
+                    self.uvs_ind.append([int(value) for value in t3.split('/')][1] - 1)
+
+                    self.normal_ind.append([int(value) for value in t1.split('/')][2] - 1)
+                    self.normal_ind.append([int(value) for value in t2.split('/')][2] - 1)
+                    self.normals.append([int(value) for value in t3.split('/')][2] - 1)
                 line = fp.readline()
+
+    def draw(self):
+        for t in range(0, len(self.triangles), 3):
+            glBegin(self.draw_type)
+            glVertex3fv(self.vertices[self.triangles[t]])
+            glVertex3fv(self.vertices[self.triangles[t + 1]])
+            glVertex3fv(self.vertices[self.triangles[t + 2]])
+            glEnd()
--- main.py
+++ main.py
@@ -64,6 +64,31 @@ def init_camera():
     glEnable(GL_DEPTH_TEST)
     camera.update(screen.get_width(), screen.get_height())
 
+## class Material >:(
+
+class Material:
+    def __init__(self, filepath):
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT)
+        image = pygame.image.load(filepath).convert()
+        image_width, image_height = image.get_rect().size
+        image_data = pygame.image.tostring(image, "RGBA")
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
+
+#######
+
 def draw_world_axes():
     glLineWidth(1)
     glBegin(GL_LINES)
